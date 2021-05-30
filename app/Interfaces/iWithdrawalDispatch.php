f1d8b5a9288cfe69f959a5490def68c9d675cc19
Emmanuel Accomford <eaccomford@gmail.com>
2021-05-30 23:22:36 +0000

    modify the withdrawal interface

--- app/Interfaces/iWithdrawalDispatch.php
+++ app/Interfaces/iWithdrawalDispatch.php
@@ -4,6 +4,6 @@ namespace App\Interfaces;
 interface iWithdrawalDispatch{
 
     public function startWithdrawal($account_number);
-    public function check($account_number);
+    public function check($account_number); 
 
 }
